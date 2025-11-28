503fd7495eb13c779720f1e99cf4f9bc0e03ad8e
Chandan-004 <chandanlmp1607@gmail.com>
2025-11-28 17:34:44 +0530

    backend production ready code

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import 'dotenv/config'
+import cors from 'cors'
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+
+import { inngest, functions } from "./inngest/index.js"
+
+const app = express();
+const PORT= process.env.PORT || 5000
+app.use(express.json());
+app.use(cors())
+app.use(clerkMiddleware())
+
+app.get('/', (req, res)=> express.send("Server is Live! "))
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.listen(PORT, ()=> console.log(`Server is running on PORT ${PORT}`))
